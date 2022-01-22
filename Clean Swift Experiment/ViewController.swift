4fedef129ca3776ae49cfe1ce4289f6d690cb2d9
Lennart Wisbar <lewisbar.git@gmail.com>
2022-01-22 18:56:23 +0100

    Initial Commit

---
+++ Clean Swift Experiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean Swift Experiment
+//
+//  Created by Lennart Wisbar on 22.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
